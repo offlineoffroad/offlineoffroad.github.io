8ff62a938206e8743860c7e6d645afc574447b18
Benjamin Parry <benjamin@parry.is>
2014-12-28 20:05:11 +0000

    Update gruntfile so it works.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,41 +1,22 @@
-// Sample grunt-jekyll grunt.js file
-// https://github.com/dannygarcia/grunt-jekyll
-
-/*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+	grunt.initConfig({
 
-    jekyll: {
-      server : {
-        src : 'templates',
-        dest: 'dev',
-        server : true,
-        server_port : 8000,
-        auto : true
-      },
-      dev: {
-        src: 'templates',
-        dest: 'dev'
-      },
-      prod: {
-        src: 'templates',
-        dest: 'prod'
-      }
-    },
+		jekyll: {
+			options: {
+				src : './',
+				dest: './_site'
+			},
 
-    watch: { // for development run 'grunt watch'
-      jekyll: {
-        files: ['templates/*.html'],
-        tasks: ['jekyll:dev']
-      }
-    }
-  });
+			serve: {
+				options: {
+					serve: true
+				}
+			},
+		}
+	});
 
-  // Default task. Run standard jekyll server.
-  grunt.registerTask('default', 'jekyll:server');
+	grunt.loadNpmTasks('grunt-jekyll');
 
-  // plugin tasks
-  grunt.loadNpmTasks('grunt-jekyll');
+	grunt.registerTask('default', ['jekyll']);
 };
